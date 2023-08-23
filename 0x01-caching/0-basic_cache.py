7c18e8c325d86567b4218954246f2a03e07452a5
1LolaOla <funmiolay1@gmail.com>
2023-08-23 10:37:14 +0000

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,30 +3,16 @@
 
 BaseCaching = __import__('base_caching').BaseCaching
 
-class BasicCache(BaseCaching):
-    """define basic cache class
 
-    Args:
-        BaseCaching (class): Parent cache class
-    """
+class BasicCache(BaseCaching):
+    """Basic Cache class"""
 
     def put(self, key, item):
-        """add data to cache_data
-
-        Args:
-            key (_type_): cache_data key
-            item (_type_): corresponding value to key
-        """
-        if key and item:
+        """add to cache"""
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """get value associated with key
-
-        Args:
-            key (_type_): cache_data key
-
-        Returns:
-            _type_: None if unsuccessful else data at key
-        """
-        return self.cache_data.get(key, None)
+        """get from cache"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
