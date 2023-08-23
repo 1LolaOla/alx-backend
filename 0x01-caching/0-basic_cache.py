ee188a7f13606c04334c9bc0129a100e96ae5855
1LolaOla <funmiolay1@gmail.com>
2023-08-23 10:17:51 +0000

    update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 """Pyton basic caching"""
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
-    """Basic Cache class"""
+    """define basic cache class
+
+    Args:
+        BaseCaching (class): Parent cache class
+    """
 
     def put(self, key, item):
-        """add to cache"""
-        if key is not None and item is not None:
+        """add data to cache_data
+
+        Args:
+            key (_type_): cache_data key
+            item (_type_): corresponding value to key
+        """
+        if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """get from cache"""
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
+        """get value associated with key
+
+        Args:
+            key (_type_): cache_data key
+
+        Returns:
+            _type_: None if unsuccessful else data at key
+        """
+        return self.cache_data.get(key)
