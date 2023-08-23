539292ca62cf9086dc7fcdd22fede42a7b43c1fe
1LolaOla <funmiolay1@gmail.com>
2023-08-23 11:21:37 +0000

    update

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """FIFO caching"""
 
-from collections import deque
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -15,11 +15,16 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """Add an item to cache"""
-        if key is not None and item is not None and key not in self.cache_data:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return key
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 first_item = self.key_queue.pop(0)
                 del self.cache_data[first_item]
-                print(f"DISCARD: {first_item}")
+                print(f 'DISCARD: {first_item}')
+
         self.cache_data[key] = item
         self.key_queue.append(key)
 
