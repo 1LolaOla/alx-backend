aa9005b09a3c82832f128ab245c4ddfdcfbc9ac6
1LolaOla <funmiolay1@gmail.com>
2023-08-29 17:15:18 +0000

    4-app.py

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Function to parametrize templates """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """config classs setup"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """get locale setup"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Route function setup"""
+    return render_template("3-index.html")
+
+
+if __name__ == '__main__':
+    """run setup"""
+    app.run(debug=True)
