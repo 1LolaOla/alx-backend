a4a69f004cf5f4a6d5fcf10ed1d169aaa36f89aa
1LolaOla <funmiolay1@gmail.com>
2023-08-29 23:53:31 +0000

    4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,34 +1,44 @@
 #!/usr/bin/python3
 """Force locale with URL parameter"""
+
+import babel
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext as _
-
-
-class Config(object):
-    """config classs"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
+from flask_babel import Babel
 
 app = Flask(__name__)
-app.config.from_object(Config)
 babel = Babel(app)
+
+
+class Config:
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
-    """get locale"""
-    lang = request.args.get('locale')
-    if lang in app.config['LANGUAGES']:
-        return lang
+    """
+    detect if the incoming request contains locale
+    argument and ifs value is a supported locale, return it
+    """
+    locale = request.args.get('locale')
+    if locale:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """Route function"""
-    return render_template("4-index.html")
+    """
+    hello world
+    """
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
